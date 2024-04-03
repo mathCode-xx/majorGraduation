154e2ad1515fc8ee58fa9c45e717ee47786e6a1d
徐鑫 <1589039158@qq.com>
2024-04-03 14:10:07 +0800

    fix: 修改mybatis删除为逻辑删除

--- src/main/java/cn/scut/xx/majorgraduation/dao/po/BasePO.java
+++ src/main/java/cn/scut/xx/majorgraduation/dao/po/BasePO.java
@@ -2,6 +2,7 @@ package cn.scut.xx.majorgraduation.dao.po;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -19,5 +20,6 @@ public class BasePO {
      * 逻辑删除字段，请见系统字典kind=2
      */
     @TableField("delete_flag")
+    @TableLogic(value = "0", delval = "1")
     private Integer deleteFlag;
 }
