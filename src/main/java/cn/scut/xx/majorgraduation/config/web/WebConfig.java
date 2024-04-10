c6a8c10a9a3c3f2d659a093c28c4a3b7103e2c2a
徐鑫 <1589039158@qq.com>
2024-04-10 08:33:51 +0800

    fix: 解决CORS

--- src/main/java/cn/scut/xx/majorgraduation/config/web/WebConfig.java
+++ src/main/java/cn/scut/xx/majorgraduation/config/web/WebConfig.java
@@ -2,6 +2,7 @@ package cn.scut.xx.majorgraduation.config.web;
 
 import cn.scut.xx.majorgraduation.core.interceptor.GlobalInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,5 +15,22 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new GlobalInterceptor());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                // 允许的源
+                .allowedOrigins("http://192.168.231.79:8080")
+                // 允许的HTTP方法
+                .allowedMethods("GET", "POST")
+                // 允许的请求头
+                .allowedHeaders("*")
+                // 允许暴露的响应头
+                .exposedHeaders("*")
+                // 是否允许发送Cookie
+                .allowCredentials(true)
+                // 预检请求的有效期，单位为秒
+                .maxAge(3600);
     }
 }
