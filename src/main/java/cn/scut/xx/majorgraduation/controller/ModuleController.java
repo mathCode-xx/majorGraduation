cea60509973871b028c3b40c0245e0d61886f6b5
徐鑫 <1589039158@qq.com>
2024-04-11 10:59:22 +0800

    feat: 获取当前用户所属module权限

--- src/main/java/cn/scut/xx/majorgraduation/controller/ModuleController.java
+++ src/main/java/cn/scut/xx/majorgraduation/controller/ModuleController.java
@@ -2,9 +2,11 @@ package cn.scut.xx.majorgraduation.controller;
 
 import cn.scut.xx.majorgraduation.core.result.Result;
 import cn.scut.xx.majorgraduation.core.result.Results;
+import cn.scut.xx.majorgraduation.dao.po.UserPO;
 import cn.scut.xx.majorgraduation.pojo.dto.req.ModuleSaveReqDTO;
 import cn.scut.xx.majorgraduation.pojo.dto.resp.ModuleRespDTO;
 import cn.scut.xx.majorgraduation.service.IModuleService;
+import cn.scut.xx.majorgraduation.service.ITokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +21,7 @@ import java.util.List;
 public class ModuleController {
 
     private final IModuleService moduleService;
+    private final ITokenService tokenService;
 
     @GetMapping("all")
     public Result<List<ModuleRespDTO>> getAll() {
@@ -34,5 +37,11 @@ public class ModuleController {
     @GetMapping("user")
     public Result<List<ModuleRespDTO>> getModuleByUser(@RequestParam("userId") Long userId) {
         return Results.success(moduleService.getModuleByUser(userId));
+    }
+
+    @GetMapping("user/current")
+    public Result<List<ModuleRespDTO>> getModuleFromCurrentUser(@RequestHeader("token") String token) {
+        UserPO user = tokenService.getUserInfoFromToken(token);
+        return Results.success(moduleService.getModuleByUser(user.getUserId()));
     }
 }
